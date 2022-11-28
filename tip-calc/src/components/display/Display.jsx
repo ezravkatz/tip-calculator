28949b38d52b3a225e1d228816bcddeb5e914aed
ezravkatz <ezravkatz@gmail.com>
2022-11-28 16:51:31 -0500

    fixed display component

--- tip-calc/src/components/display/Display.jsx
+++ tip-calc/src/components/display/Display.jsx
@@ -1,23 +1,31 @@
-import React from 'react'
-import styles from './display.css'
+import React from "react";
+import styles from "./display.css";
 
 const Display = () => {
   return (
-    <div>
-      <div className="display__row">
-        <div>
-        <div className="display__label">
-          <p className='tip__header'>Tip Amount</p>
-          <p className='unit'> Person</p>
-        </div>
-        <div className="display__amt">
-          <p className='tip__value'>$0.00</p>
-        </div>
-      </div>
-      <button className='btn'>Reset</button>
-      </div>
-    </div>
-  )
-}
+    <div className="display">
+      <div className="display__group">
+        <div className="display__row">
+          <div className="display__label">
+            <p className="tip__header">Tip Amount</p>
+            <p className="unit">/ Person</p>
+          </div>
 
-export default Display
+          <p className="display__amt">$0.00</p>
+        </div>
+        <div className="display__row">
+          <div className="display__label">
+            <p className="header"> Total</p>
+            <p className="unit">/ Person</p>
+          </div>
+
+          <p className="display__amt">$0.00</p>
+        </div>
+      </div>
+
+      <button className="btn">Reset</button>
+    </div>
+  );
+};
+
+export default Display;
