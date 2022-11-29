bd7d4c35d1406abd495374abcab6fae0b0895040
ezravkatz <ezravkatz@gmail.com>
2022-11-29 10:09:49 -0500

    styling fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
+// import { useState } from "react";
 
 //import images
-import logo from "./assets/images/logo1.png";
+import logo from "./assets/images/logo3.png";
 
 // style
 import "./index.css";
@@ -11,17 +11,19 @@ import Display from "./components/display/Display";
 import Form from "./components/form/Form";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
-    <div className="wrapper">
-      <img src={logo} alt="Tip Calc Logo" />
-      <div className="container">
-        <Form />
-        <Display />
+    <body>
+      <div className="wrapper">
+        <img src={logo} alt="Tip Calc Logo" />
+        <div className="container">
+          <Form />
+          <Display />
+        </div>
+        <div className="display"></div>
       </div>
-      <div className="display"></div>
-    </div>
+    </body>
   );
 }
 
--- src/components/display/Display.jsx
+++ src/components/display/Display.jsx
@@ -1,26 +1,24 @@
 import React from "react";
-import styles from "./display.css";
+import "./display.css";
 
 const Display = () => {
   return (
-    <div className="display">
-      <div className="display__group">
-        <div className="display__row">
-          <div className="display__label">
-            <p className="tip__header">Tip Amount</p>
-            <p className="unit">/ Person</p>
-          </div>
-
-          <p className="display__amt">$0.00</p>
+    <div id="display">
+      <div className="display__row">
+        <div className="display__label">
+          <p className="tip__header">Tip Amount</p>
+          <p className="unit">/ Person</p>
         </div>
-        <div className="display__row">
-          <div className="display__label">
-            <p className="header"> Total</p>
-            <p className="unit">/ Person</p>
-          </div>
 
-          <p className="display__amt">$0.00</p>
+        <p className="display__amt">$0.00</p>
+      </div>
+      <div className="display__row">
+        <div className="display__label">
+          <p className="header"> Total</p>
+          <p className="unit">/ Person</p>
         </div>
+
+        <p className="display__amt">$0.00</p>
       </div>
 
       <button className="btn" disabled>
