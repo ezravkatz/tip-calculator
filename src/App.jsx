d8baa62791a855b5b5adfe4787f8be11ea0ad94b
ezravkatz <ezravkatz@gmail.com>
2022-11-29 05:34:21 -0500

    rearranged file structure

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+//import images
+import logo from "./assets/images/logo1.png";
+
+// style
+import "./index.css";
+
+// components
+import Display from "./components/display/Display";
+import Form from "./components/form/Form";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="wrapper">
+      <img src={logo} alt="Tip Calc Logo" />
+      <div className="container">
+        <Form />
+        <Display />
+      </div>
+      <div className="display"></div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/display/Display.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import styles from "./display.css";
+
+const Display = () => {
+  return (
+    <div className="display">
+      <div className="display__group">
+        <div className="display__row">
+          <div className="display__label">
+            <p className="tip__header">Tip Amount</p>
+            <p className="unit">/ Person</p>
+          </div>
+
+          <p className="display__amt">$0.00</p>
+        </div>
+        <div className="display__row">
+          <div className="display__label">
+            <p className="header"> Total</p>
+            <p className="unit">/ Person</p>
+          </div>
+
+          <p className="display__amt">$0.00</p>
+        </div>
+      </div>
+
+      <button className="btn" disabled>
+        Reset
+      </button>
+    </div>
+  );
+};
+
+export default Display;
